3762c1820eed9e5cc4bb91b0ef1d385c999e62ec
Esther Min <estherjmin@gmail.com>
2020-12-18 17:53:14 -0500

    setup initial ajax call to retrieve recipe data

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 $('#searchBtn').on('click', function (event) {
     event.preventDefault();
 
+    // Variables
     let searchInput = $('#search-input').val().trim();
 
     // Exit the function if the search input is empty
@@ -10,4 +11,16 @@ $('#searchBtn').on('click', function (event) {
     }
 
     console.log(searchInput);
+
+    // API Variables
+    let APIKey = '60cfc48a630f93872e74e6d14339f9ce';
+    let queryURL = 'https://api.edamam.com/search?q=' + searchInput + '&app_id=cc3b021a&app_key=' + APIKey;
+
+    // AJAX call to retrieve recipe data
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    }).then(function (recipeData) {
+        console.log(recipeData);
+    })
 })
