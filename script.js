5896fb16f20ae8da194b7a046bff1c2e6ef43ad1
Esther Min <estherjmin@gmail.com>
2020-12-21 20:20:14 -0500

    set up recipe cards to be created dynamically and to populate ingredients for each recipe

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ $('#searchBtn').on('click', function (event) {
     let healthSelections = $('.health-selections');
     let healthSelectionsArr = [];
 
-    for (let i=0; i < healthSelections.length; i++) {
+    for (let i = 0; i < healthSelections.length; i++) {
         if (healthSelections[i].checked) {
             healthSelectionsArr.push(healthSelections[i].value);
         }
@@ -36,12 +36,47 @@ $('#searchBtn').on('click', function (event) {
         let recipe = recipeData.hits;
         console.log(recipe);
 
-        let recipeName = recipe[i].label;
-        let image = recipe[i].image;
-        let yield = recipe[i].yield;
-        let cookTime = recipe[i].totalTime;
-        let ingredients = recipe[i].ingredients;
-        let directions = recipe[i].url
+        // Loop through the data and create a new recipe card for each recipe
+        for (let i = 0; i < recipe.length; i++) {
+            let recipeName = recipe[i].label;
+            // let image = recipe[i].image;
+            let yield = recipe[i].yield;
+            let cookTime = recipe[i].totalTime;
+            let ingredientsArr = recipe[i].recipe.ingredients;
+            let directions = recipe[i].url
 
+            // Create new divs
+            let recipeCardDiv = $("<div>");
+            let infoDiv = $("<div>");
+            let ingredientsDiv = $("<div>");
+            let instructionsDiv = $("<div>");
+
+            // Set classes for new divs
+            recipeCardDiv.attr("class", "card col-12 col-md-5 col-lg-3");
+            infoDiv.attr("class", "card-body");
+            ingredientsDiv.attr("class", "card-body pt-0");
+            instructionsDiv.attr("class", "card-body d-flex justify-content-center align-items-center p-0");
+
+            // Create image for recipe
+            let recipeImage = $("<img>").attr("src", recipe[i].image);
+            recipeImage.attr("class", "card-img-top");
+
+            recipeCardDiv.append(recipeImage, infoDiv, ingredientsDiv, instructionsDiv);
+
+
+            // Create unordered lists for ingredients and append to the ingredientsDiv
+            let ingredientsTitle = $("<p>").attr("class", "card-text strong m-0").text("Ingredients:");
+            let ingredientsUL = $("<ul>");
+
+            for (let i = 0; i < ingredientsArr.length; i++) {
+                ingredientsUL.append($("<li>").text(ingredientsArr[i].text));
+            }
+
+            ingredientsDiv.append(ingredientsTitle, ingredientsUL);
+
+            console.log()
+
+            $("#search-results").append(recipeCardDiv);
+        }
     })
 })
